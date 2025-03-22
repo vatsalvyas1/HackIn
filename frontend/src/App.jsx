43ba3852c18c5a400df57064da7026c9c4870692
Rajat Mangla <rajatmangla0203@gmail.com>
2025-03-22 13:39:57 +0530

    Remove SignUp component and update backend URL import in firebase.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
-import SignUp from './components/SignUp.jsx'
 import NavBar from './components/Navbar.jsx'
 import HomePage from './components/HomePage.jsx'
 import Footer from './components/Footer.jsx'
@@ -20,7 +19,6 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path="/" element={<Link to={"/login"}>Login</Link>} />
-        <Route path="/signup" element={<SignUp />} />
         <Route path='/edit-profile' element={<EditProfile />} />
         <Route path='/dashboard' element={<DashBoard />} />
         <Route path='/add-project' element={<AddProject />} />
